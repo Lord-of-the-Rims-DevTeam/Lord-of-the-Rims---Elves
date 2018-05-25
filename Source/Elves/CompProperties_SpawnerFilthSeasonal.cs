64b22c8d552c651a114a59faf46678bfc1a5898b
jecrell <matt.walls31@gmail.com>
2018-05-25 10:27:48 +0900

    Added seasonal spawner to make sure Mallorn trees drop their leaves in the springtime

---
+++ Source/Elves/CompProperties_SpawnerFilthSeasonal.cs
@@ -0,0 +1,26 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Elves
+{
+    public class CompProperties_SpawnerFilthSeasonal : CompProperties
+    {
+        public CompProperties_SpawnerFilthSeasonal()
+        {
+            this.compClass = typeof(CompSpawnerFilthSeasonal);
+        }
+
+        public ThingDef filthDef;
+
+        public int spawnCountOnSpawn = 5;
+
+        public float spawnMtbHours = 12f;
+
+        public float spawnRadius = 3f;
+
+        public float spawnEveryDays = -1f;
+
+        public RotStage? requiredRotStage;
+        public Season? requiredSeason;
+    }
+}
